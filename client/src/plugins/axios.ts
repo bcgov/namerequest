b33ce4574938727d42295f80ac1cfa88848e0283
Scott Rumsby <scott.rumsby@gmail.com>
2020-02-03 16:45:27 -0800

    Fixed issue with app either not working locally or not deploying that was related to the server root.  Fixed the bad CSS import in vue.config.js.  Reset the router base back to BASE_URL env variable.  all tests passing.

--- client/src/plugins/axios.ts
+++ client/src/plugins/axios.ts
@@ -2,9 +2,7 @@ import axios from 'axios'
 
 let baseURL = function () {
   if (process.env.NODE_ENV === 'development') {
-    if (process.env.VUE_APP_MOCK_API === 'yes') {
-      return process.env.VUE_APP_API_URL_MOCK
-    }
+    return process.env.VUE_APP_API_URL_MOCK
   }
   return process.env.VUE_APP_API_URL
 }
