af896e8fb8bbc6135a1ed8e70ec73b2d6851b9f4
Scott Rumsby <scott.rumsby@gmail.com>
2020-02-03 16:45:27 -0800

    resolved sass import issue that was preventing the app from compiling when running off the server root and reconfigured the .env files to hold the API_URLs.  Re-committed public/config/configuration.json and set the app's public path to /namerequest in all environments

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,17 +2,19 @@ const path = require("path")
 
 module.exports = {
   configureWebpack: {
-    devtool: 'eval-source-map',
+    devtool: 'eval-source-map'
   },
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'sass',
-      patterns: './src/sass/theme.sass'
+      patterns: [
+        path.resolve(__dirname, './src/sass/theme.sass')
+      ]
     }
   },
   transpileDependencies: [
     'vuetify',
     'vuex-module-decorators'
   ],
-  publicPath: ''
+  publicPath: process.env.VUE_APP_PATH
 }
