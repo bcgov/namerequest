b4efe66253986204368b20a6d9bfd5b9923f7f3e
Severin Beauvais <severin.beauvais@gmail.com>
2020-10-29 16:34:32 -0700

    - deleted package - removed Business Structures Tool from Search component - removed obsolete CSS override

--- client/src/components/new-request/search.vue
+++ client/src/components/new-request/search.vue
@@ -1,17 +1,7 @@
 <template>
   <v-container fluid id="new-request-container" class="copy-normal">
     <v-row justify="end">
-      <v-col cols="6" class="copy-normal">I need a name to:</v-col>
-      <v-col cols="6" style="display: flex; justify-content: flex-end">
-        <business-structures-tool>
-          <template #start="{ homeModal }">
-             <div id="#help-me-choose-activator"
-                  class="link-std"
-                  style="background-color: white"
-                  @click="homeModal()">Help Me Choose</div>
-          </template>
-        </business-structures-tool>
-      </v-col>
+      <v-col cols="12" class="copy-normal">I need a name to:</v-col>
       <v-col cols="5">
         <!--request_action_cd-->
         <v-select :error-messages="errors.includes('request_action_cd') ? 'Please select a type' : ''"
@@ -103,7 +93,6 @@ import newReqModule from '../../store/new-request-module'
 import { bcMapping, xproMapping } from '@/store/list-data/request-action-mapping'
 import { Component, Vue, Watch } from 'vue-property-decorator'
 import { LocationT } from '@/models'
-import '@bizpal/open-services-bst/'
 
 @Component({
   components: { NameInput }
