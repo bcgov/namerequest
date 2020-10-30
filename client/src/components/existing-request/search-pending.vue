abf67e0f821696e7640a911456373d21c8bf457b
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2020-10-30 08:39:14 -0700

    - created new Search Pending component (#133)
    - display new component on Find Name Request
    - misc cleanup

---
+++ client/src/components/existing-request/search-pending.vue
@@ -0,0 +1,40 @@
+<template>
+  <MainContainer id="search-pending-container">
+    <template v-slot:container-header>
+      <v-col cols="auto h4">
+        You are searching for an existing Name Request
+      </v-col>
+    </template>
+
+    <template v-slot:content>
+      <v-row justify="center">
+        <v-col cols="auto">
+          <b>Please wait a moment</b>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-progress-circular color="orange"
+                               id="analyze-pending-spinner"
+                               size="50"
+                               indeterminate />
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="auto" class="copy-small">
+          Search Status: Searching
+        </v-col>
+      </v-row>
+    </template>
+  </MainContainer>
+</template>
+
+<script lang="ts">
+import MainContainer from '@/components/new-request/main-container.vue'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: { MainContainer }
+})
+export default class SearchPending extends Vue {}
+</script>
