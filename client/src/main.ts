c4e00098b180a0a3f68b0be2b1b44a05ff1d6597
Cameron Bowler <cameron@freshworks.io>
2020-11-02 10:39:10 -0800

    Reverted Changes and swapped icon library

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,13 +3,12 @@ import App from './App.vue'
 import { getVueRouter } from '@/router'
 import store from './store'
 import { getConfig } from '@/plugins/getConfig'
-import Vuetify from "vuetify/lib"
+import vuetify from '@/plugins/vuetify'
 import KeyCloakService from 'sbc-common-components/src/services/keycloak.services'
 
 // NB: order matters - do not change
 import 'quill/dist/quill.core.css'
 import '@mdi/font/css/materialdesignicons.min.css' // ensure you are using css-loader
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@/sass/main.sass'
 import '@/sass/overrides.sass'
 
@@ -21,8 +20,6 @@ import * as mapping from '@/store/list-data/request-action-mapping'
 
 Vue.config.productionTip = true
 Vue.config.devtools = true
-
-Vue.use(Vuetify)
 
 async function startVue () {
   Vue.prototype.$designations = designations
@@ -37,7 +34,7 @@ async function startVue () {
   await KeyCloakService.setKeycloakConfigUrl(sessionStorage.getItem('KEYCLOAK_CONFIG_PATH'))
 
   new Vue({
-    vuetify: new Vuetify({ iconfont: 'mdi' }),
+    vuetify: vuetify,
     router: getVueRouter(),
     store,
     render: h => h(App)
