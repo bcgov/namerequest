45d04c1264da5252aa76dba2cbb9ad9f56d60bf6
vysakh-menon-aot <vysakh.menon@aot-technologies.com>
2020-11-27 09:33:39 -0800

    review comments

--- client/src/components/lower-info-area/new-and-better-way.vue
+++ client/src/components/lower-info-area/new-and-better-way.vue
@@ -27,9 +27,7 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
-@Component({
-  components: {}
-})
+@Component
 export default class NewAndBetterWay extends Vue {
 }
 </script>
