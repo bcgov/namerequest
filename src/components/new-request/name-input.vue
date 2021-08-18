d3f72a2f468561dd2df15cfb32ed3a45a3866fac
eve-git <eve.deng@gov.bc.ca>
2021-08-18 09:16:24 -0700

    sanitize name input value, including removing leading and trailing spaces

--- src/components/new-request/name-input.vue
+++ src/components/new-request/name-input.vue
@@ -3,6 +3,7 @@
     <v-col cols="12" class="pa-0">
       <v-text-field :error-messages="message"
                     @input="clearErrors()"
+                    @blur="handleBlur($event)"
                     @keydown.enter="handleSubmit"
                     autocomplete="chrome-off"
                     :filled="!isReadOnly"
@@ -25,6 +26,7 @@ import { Component, Prop, Vue, Watch, Emit } from 'vue-property-decorator'
 import { Getter, Action } from 'vuex-class'
 import { Location, RequestCode } from '@/enums'
 import { ActionBindingIF } from '@/interfaces/store-interfaces'
+import { sanitizeName } from '@/plugins'
 
 @Component({})
 export default class NameInput extends Vue {
@@ -117,6 +119,11 @@ export default class NameInput extends Vue {
 
   clearErrors () {
     this.setClearErrors(null)
+  }
+
+  handleBlur (event) {
+    let value = sanitizeName(event.target.value)
+    this.setName(value)
   }
 
   async handleSubmit (event: KeyboardEvent) {
