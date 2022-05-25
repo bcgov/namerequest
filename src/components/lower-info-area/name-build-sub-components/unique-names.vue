5e8c3b839ef1a5176454b45b7da92405314944da
eve-git <54647458+eve-git@users.noreply.github.com>
2022-05-25 10:49:30 -0700

    add groups_of_similar_descriptive_words.pdf to main page (#524)
    * add groups_of_similar_descriptive_words.pdf to main page
    
    * add groups_of_similar_descriptive_words.pdf to main page
    
    * code review
    
    * code review
    
    * updated according to the UX comments
    
    * updated according to the UX comments
    
    Co-authored-by: eve-git <evedeng@NE021325.idir.BCGOV>

--- src/components/lower-info-area/name-build-sub-components/unique-names.vue
+++ src/components/lower-info-area/name-build-sub-components/unique-names.vue
@@ -31,6 +31,15 @@
         <strong>Westcoast Barbers Inc.</strong>
       </p>
     </v-row>
+
+    <v-divider class="ml-0 mb-4"></v-divider>
+
+    <v-row class="ml-1 mb-4">
+      <a href="./pdfs/groupings_of_similar_descriptive_words.pdf" target="_blank" class="link-std-sans-ul">
+        <v-icon color="primary">mdi-file-pdf-outline</v-icon>
+        Download Groupings of Similar Descriptive Words
+      </a>
+    </v-row>
   </v-container>
 </template>
 
