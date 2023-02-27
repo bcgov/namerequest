e27aa20c9366f1a939f2798368a7c01147c7812f
JazzarKarim <karim.jazzar@gov.bc.ca>
2023-02-27 12:49:35 -0800

    15183 Imported Genesys Web Chat

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,13 @@
                  :webChatStatusUrl = "window['webChatStatusUrl']"
       />
 
+      <!-- Display the Genesys WebMessage -->
+      <GenesysWebMessage
+        genesysURL="https://apps.cac1.pure.cloud/genesys-bootstrap/genesys.min.js"
+        environmentKey="cac1"
+        deploymentKey="5c012aaa-1f17-4d0b-8da2-bea9c1133d5d"
+      />
+
       <!-- Loading spinner -->
       <v-fade-transition>
         <div class="loading-container grayed-out" v-show="showSpinner">
@@ -86,6 +93,7 @@ import { DateMixin, LoadKeycloakRolesMixin, NrAffiliationMixin, UpdateUserMixin 
 import axios from 'axios'
 
 // dialogs and other components
+import { GenesysWebMessage } from '@bcrs-shared-components/genesys-web-message'
 import { WebChat as ChatPopup } from '@bcrs-shared-components/web-chat'
 import {
   AffiliationErrorDialog, CancelDialog, ConditionsDialog, ErrorDialog, ExitDialog, HelpMeChooseDialog,
@@ -112,6 +120,7 @@ import { PAYMENT_REQUIRED } from 'http-status-codes'
     ErrorDialog,
     ExitDialog,
     ExitIncompletePaymentDialog,
+    GenesysWebMessage,
     HelpMeChooseDialog,
     LocationInfoDialog,
     MrasSearchInfoDialog,
