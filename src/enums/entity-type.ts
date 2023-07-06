4b50d3765d4d94d622ca0e2e544d3c97dc01fac4
Severin Beauvais <severin.beauvais@gmail.com>
2023-07-06 08:46:54 -0700

    - updated browserslist config - app version = 5.0.3 - added corp-type-module dependency - mapped EntityType to CorpTypeCd

--- src/enums/entity-type.ts
+++ src/enums/entity-type.ts
@@ -1,29 +1,31 @@
+import { CorpTypeCd } from '@bcrs-shared-components/corp-type-module'
+
 /** Enum for Entity Type codes. */
 export enum EntityType {
   // BC Entity Types:
-  BC = 'BC',
-  CC = 'CC',
-  CP = 'CP',
-  CR = 'CR',
-  DBA = 'DBA',
-  FI = 'FI',
-  FR = 'FR',
-  GP = 'GP',
-  LL = 'LL',
-  LP = 'LP',
-  PA = 'PA',
-  PAR = 'PAR',
+  BC = CorpTypeCd.BC_COMPANY,
+  CC = CorpTypeCd.BC_CCC,
+  CP = CorpTypeCd.COOP,
+  CR = CorpTypeCd.BC_CORPORATION,
+  DBA = CorpTypeCd.DOING_BUSINESS_AS,
+  FI = CorpTypeCd.FINANCIAL,
+  FR = CorpTypeCd.NR_SOLE_PROP,
+  GP = CorpTypeCd.PARTNERSHIP,
+  LL = CorpTypeCd.LL_PARTNERSHIP,
+  LP = CorpTypeCd.LIM_PARTNERSHIP,
+  PA = CorpTypeCd.PRIVATE_ACT,
+  PAR = CorpTypeCd.PARISHES,
   SO = 'SO',
-  SP = 'SP',
-  UL = 'UL',
+  SP = CorpTypeCd.SOLE_PROP,
+  UL = CorpTypeCd.BC_UNLIMITED,
 
   // XPRO Entity Types:
-  XCR = 'XCR',
+  XCR = CorpTypeCd.XPRO_CORPORATION,
   XUL = 'XUL',
   RLC = 'RLC',
   XLP = 'XLP',
   XLL = 'XLL',
-  XCP = 'XCP',
+  XCP = CorpTypeCd.XPRO_COOP,
   XSO = 'XSO',
 
   INFO = 'INFO', // special value for sub-menu
