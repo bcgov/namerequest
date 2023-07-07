9a867b33a6aa5b7cdbc7ca41e3add7e075ab4f9f
Leo Dube <leo.dube@aot-technologies.com>
2023-07-07 06:14:36 -0700

    Undo change tab width

--- src/components/tabs.vue
+++ src/components/tabs.vue
@@ -94,14 +94,6 @@ export default class Tabs extends Mixins(CommonMixin) {
   background-color: $BCgovBlue5;
 }
 
-#new-tab {
-  width: 67%;
-}
-
-#existing-tab {
-  width: 33%;
-}
-
 .upper-border {
   border-top-left-radius: 4px;
   border-top-right-radius: 4px;
