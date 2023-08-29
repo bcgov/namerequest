1bccb2ccf5ecf510135f3e0ef71c8799ebd0da8e
Severin Beauvais <severin.beauvais@gmail.com>
2023-08-29 14:17:39 -0700

    - fixed premature validation - added Tab event handler

--- src/components/new-request/business-fetch.vue
+++ src/components/new-request/business-fetch.vue
@@ -5,19 +5,18 @@
       append-icon="mdi-magnify"
       autocomplete="chrome-off"
       autofocus
-      :error-messages="errorMessages"
       filled
-      @keyup.enter="onItemSelected()"
-      @click:append="onItemSelected()"
-      :loading="state === States.SEARCHING"
-      :name="Math.random()"
       hide-details="auto"
       hint="Enter registration number of existing business"
       label="Fetch an existing business"
       persistent-hint
-      ref="searchField"
-      :rules="rules"
       v-model="searchField"
+      @click:append="onItemSelected()"
+      @keydown.tab.exact="onItemSelected()"
+      @keyup.enter="onItemSelected()"
+      :error-messages="errorMessages"
+      :loading="state === States.SEARCHING"
+      :name="Math.random()"
     />
   </div>
 </template>
@@ -26,7 +25,7 @@
 import Vue from 'vue'
 import { Component, Emit } from 'vue-property-decorator'
 import { Action } from 'vuex-class'
-import { BusinessFetchIF, FormType } from '@/interfaces'
+import { BusinessFetchIF } from '@/interfaces'
 
 enum States {
   INITIAL = 'initial',
@@ -42,12 +41,7 @@ export default class BusinessFetch extends Vue {
   // Enum for template
   readonly States = States
 
-  // Refs
-  $refs!: {
-    searchField: FormType
-  }
-
-  // Store actions
+  // Store action
   @Action fetchCorpNum!: (corpNum: string) => Promise<BusinessFetchIF>
 
   /** V-model for search field. */
@@ -59,31 +53,26 @@ export default class BusinessFetch extends Vue {
   /** State of this component. */
   state = States.INITIAL
 
-  /** Validation rules. */
-  get rules (): Array<any> {
-    function isValidCorpNum (corpNum: string): boolean {
-      return (corpNum && corpNum.length === 9)
+  /** Validates the search field, sets any error messages, and returns validity. */
+  private validate (): boolean {
+    if (!this.searchField) {
+      this.errorMessages = ['Required field']
+    } else if (!/^(A|BC|C|CP|FM)\d{7}$/.test(this.searchField)) {
+      this.errorMessages = ['Please enter a valid registration number']
+    } else {
+      this.errorMessages = []
     }
-
-    return [
-      v => !!v || 'Required field',
-      v => isValidCorpNum(v) || 'Please enter a valid registration number'
-    ]
+    return (this.errorMessages.length === 0)
   }
 
   /** When an item has been selected, emits event with business object. */
-  // *** TODO: fix validation issues
   @Emit('business')
   async onItemSelected (): Promise<BusinessFetchIF> {
-    console.log(`*** validating = [${this.searchField}]`)
-    const valid = this.$refs.searchField.validate()
+    // validate search field
+    const valid = this.validate()
     if (!valid) return
 
     // perform search
-    // *** BC0870000 - in COLIN only
-    // *** BC0871000 - does not exist
-    // *** BC0871408 - in LEAR
-    // *** FM1041131 - in LEAR
     this.state = States.SEARCHING
     const result = await this.fetchCorpNum(this.searchField).catch(e => null)
 
