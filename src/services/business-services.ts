b84829586f0e23404b344c43d49c7d8a7899e726
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2023-08-30 08:50:01 -0700

    17489 Incremental business fetch + bit of cleanup (#663)
    * - changed Legal Api to V2
    - app version = 5.0.30
    - added max-height support to Nested Select component
    - added call to fetch corp num
    - added error handling
    - increased max height of request action menu to partially show last option (UX feedback)
    - simplified business lookup/search template code
    - added business event handler + misc fallbacks
    - deleted unused business interfaces + added interface for business fetch
    - commented out interceptor debugging message
    - moved searchEntities and searchColin code to namex-services.ts
    - fixed a bunch of function signatures in actions.ts
    - refactored checkCOLIN() for simplicity and better return object
    - fixed a bug in submit()
    
    * - also set corp num when a business is searched
    
    * - fixed premature validation
    - added Tab event handler
    
    * - trim search field (in case of whitespace)
    
    * - cleared errors and reset state when user changes input
    - set readonly while searching
    - updated error message
    - don't auto-hide "not found" error

--- src/services/business-services.ts
+++ src/services/business-services.ts
@@ -8,13 +8,23 @@ export default class BusinessServices {
   /** Legal API URL. */
   static legalApiUrl = sessionStorage.getItem('LEGAL_API_URL')
 
-  /** Creates a temporary (draft) business record. */
+  /**
+   * Creates (posts) a draft (temporary) business record.
+   * Must be logged in to use this.
+   * Throws an exception on error.
+   */
   static async createBusiness (businessRequest: BusinessRequest): Promise<any> {
-    return axios.post(`${BusinessServices.legalApiUrl}/businesses?draft=true`, businessRequest)
+    const url = `${BusinessServices.legalApiUrl}/businesses?draft=true`
+    return axios.post(url, businessRequest)
   }
 
-  /** Fetches a business record. */
-  static async fetchBusiness (businessId: number): Promise<any> {
-    return axios.get(`${BusinessServices.legalApiUrl}/businesses/${businessId}`)
+  /**
+   * Fetches a business record.
+   * Must be logged in to use this.
+   * Throws an exception on error.
+   */
+  static async fetchBusiness (identifier: string): Promise<any> {
+    const url = `${BusinessServices.legalApiUrl}/businesses/${identifier}`
+    return axios.get(url).then(response => response.data?.business)
   }
 }
