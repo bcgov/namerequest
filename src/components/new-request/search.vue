7d6cc2d3c84ad63cafeb527c43d34ef8c2a452c6
ketaki-deodhar <116035339+ketaki-deodhar@users.noreply.github.com>
2023-09-15 11:06:47 -0700

    17727- Fix Continuation In flow bug (#695)
    * 17727-Change name flow 1
    
    * 17727-Change name flow 2
    
    * 17727-Change name flow 3
    
    * 17727-Change name flow 4
    
    * 17727-Update Continuation In flow 1
    
    * 17727-Update Continuation In flow 2
    
    * 17727-Update Continuation In flow 3
    
    * 17727-Update Continuation In flow 4
    
    * 17727-Update Continuation In flow 5
    
    * 17727-Update Continuation In fix check name button not shown
    
    * 17727-fix merge issues

--- src/components/new-request/search.vue
+++ src/components/new-request/search.vue
@@ -364,13 +364,6 @@ export default class Search extends Mixins(CommonMixin, NrAffiliationMixin, Sear
 
   /** Whether to show "Go to COLIN" button (otherwise will show `actionNowButtonText` button). */
   get showColinButton (): boolean {
-    // Conditional for Continuation In Flow.
-    if (
-      this.isContinuationIn &&
-      this.isNumberedCompany &&
-      !this.isSupportedContinuationIn(this.getEntityTypeCd)
-    ) return true
-
     // Conditional for Amalgamation Flow.
     if (
       this.isAmalgamation &&
@@ -420,6 +413,13 @@ export default class Search extends Mixins(CommonMixin, NrAffiliationMixin, Sear
       !this.isSupportedContinuationIn(this.getEntityTypeCd)
     ) return true
 
+    // Conditional for Continuation In Flow.
+    if (
+      this.isContinuationIn &&
+      this.isNumberedCompany &&
+      !this.isSupportedContinuationIn(this.getEntityTypeCd)
+    ) return true
+
     return false
   }
 
@@ -435,12 +435,6 @@ export default class Search extends Mixins(CommonMixin, NrAffiliationMixin, Sear
   }
 
   get showCheckNameButton (): boolean {
-    // Conditional for Continuation In Flow.
-    if (this.isContinuationIn) {
-      if (this.getEntityTypeCd && this.isNamedCompany) return true
-      if (this.getEntityTypeCd && this.isSociety) return true
-    }
-
     // Conditional for "New BC-based business" Flow.
     // Show button if we're in "Start a new BC-based business" and non-numbered entity is selected.
     if (this.isNewBcBusiness) {
@@ -482,8 +476,9 @@ export default class Search extends Mixins(CommonMixin, NrAffiliationMixin, Sear
 
     // Conditional for Continuation In Flow.
     if (this.isContinuationIn) {
-      if (this.getEntityTypeCd && this.isNamedCompany) return true
+      if (this.getEntityTypeCd && !this.isNumberedEntityType) return true
       if (this.getEntityTypeCd && !this.isNumberedEntityType && this.isSociety) return true
+      if (this.getEntityTypeCd && this.isNamedCompany) return true
     }
     return false
   }
