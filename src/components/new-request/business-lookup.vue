b1354382b7b1c13486825832dcd8d81ca2407200
James <james.paolo.p.garcia@gmail.com>
2023-09-27 14:53:26 -0700

    Fixed. For PR.

--- src/components/new-request/business-lookup.vue
+++ src/components/new-request/business-lookup.vue
@@ -1,10 +1,12 @@
 <template>
   <!-- once in Summary state, need to re-mount to reuse this component -->
   <div
-    v-if="state !== States.SUMMARY"
-    id="business-lookup"
-  >
+  v-if="state !== States.SUMMARY"
+  id="business-lookup"
+>
     <v-autocomplete
+      filled
+      no-filter
       :hide-no-data="state != States.NO_RESULTS"
       :items="searchResults"
       :loading="state === States.SEARCHING"
@@ -13,15 +15,12 @@
       append-icon="mdi-magnify"
       autocomplete="chrome-off"
       autofocus
-      filled
       hint="Search by name, incorporation or registration number of an existing business"
-      item-text="name"
-      item-value="identifier"
-      :label="lookupLabel"
+      item-text="identifier"
       hide-details="auto"
-      no-filter
       persistent-hint
       return-object
+      :label="lookupLabel"
       @input="onItemSelected($event)"
       @keydown.enter.native.prevent
     >
@@ -42,6 +41,26 @@
         <p class="pl-5">
           Ensure you have entered the correct business name or number.
         </p>
+      </template>
+      <!-- Customize the results template -->
+      <template #item="{ item }">
+        <v-row class="business-lookup-result pt-1">
+          <v-col cols="3">
+            <div class="result-identifier">
+              {{ item.identifier }}
+            </div>
+          </v-col>
+          <v-col cols="7">
+            <div class="result-name">
+              {{ item.name }}
+            </div>
+          </v-col>
+          <v-col cols="2">
+            <div class="result-btn">
+                Select
+            </div>
+          </v-col>
+        </v-row>
       </template>
     </v-autocomplete>
   </div>
@@ -177,7 +196,9 @@ p {
     color: $app-green;
   }
 }
-
+.result-btn {
+  color: rgb(0, 162, 255)
+}
 // prevent Magnify icon from being rotated when list is displayed
 ::v-deep .v-input__icon .mdi-magnify {
   -webkit-transform: none !important;
