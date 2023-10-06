7126066d3fc5b51ef7f6c3c14b785b0c5ee1ed22
jamespaologarcia <james.paolo.p.garcia@gmail.com>
2023-10-05 17:21:10 -0700

    18023 incorrect name in breadcrumb (#725)
    * For PR - Breadcrumb fixed for logged in users
    
    * Removed unnecessary console log
    
    * Removed unnecessary getter
    
    * Updated the URL links for the breadcrumbs
    
    * Blank lines restored and added for consistency.
    
    * Updated comment and variable name

--- src/App.vue
+++ src/App.vue
@@ -111,7 +111,8 @@ import { Routes } from '@/enums'
 import { BreadcrumbIF } from '@/interfaces'
 import {
   getRegistryDashboardBreadcrumb,
-  getStaffDashboardBreadcrumb
+  getStaffDashboardBreadcrumb,
+  getRegistryHomeBreadcrumb
 } from '@/resources'
 import axios from 'axios'
 
@@ -172,6 +173,7 @@ export default class App extends Mixins(
   @Getter getNrId!: number
   @Getter isRoleStaff!: boolean
   @Getter isMobile!: boolean
+  @Getter isRoleBasic!: boolean
 
   // Global actions
   @Action resetAnalyzeName!: ActionBindingIF
@@ -225,11 +227,13 @@ export default class App extends Mixins(
 
     // Set base crumbs based on user role
     // Staff don't want the home landing page and they can't access the Manage Business Dashboard
+
     if (this.isRoleStaff) {
-      // If staff, set StaffDashboard as home crumb
       crumbs.unshift(getStaffDashboardBreadcrumb())
+    } else if (this.isRoleBasic) {
+      crumbs.unshift(getRegistryHomeBreadcrumb())
     } else {
-      // For non-staff, set Home crumb
+      // If not logged in, set Registry Home Breadcrumb
       crumbs.unshift(getRegistryDashboardBreadcrumb())
     }
     return crumbs
