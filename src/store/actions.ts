ba0d5fa11cc9a2b420ce2d388022304891795f00
Rajandeep98 <144159721+Rajandeep98@users.noreply.github.com>
2023-10-31 14:37:24 -0700

    18068 - Namerequest UI: Editing the Resubmit NR has the first name choice as empty. (#740)
    * updated condition for SP/GP resubmission
    
    * Updated designation checking with designation type
    
    * Simplified conditional and updated flag name
    
    * version up

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -489,14 +489,15 @@ export const resubmit = async ({ commit, getters }): Promise<boolean> => {
 
   // add resubmit NR number (for internal use only - API ignores it)
   nrTypeData['resubmitNrNum'] = nrData['nrNum']
-
+  // check against entity types if it should have designation or not : if length returns to be 0 then NR should not have any designation otherwise work as getNameDesignation
+  const checkEntityType = Designations[nrData['entity_type_cd']].words.length === 0
   // clean names data
   nrTypeData['names'] = nrData.names.map(name => ({
     choice: name.choice,
     consent_words: '',
     conflict1: '',
     conflict1_num: '',
-    designation: getNameDesignation(name),
+    designation: !checkEntityType ? getNameDesignation(name) : '',
     name: name.name,
     name_type_cd: name.name_type_cd
   }))
