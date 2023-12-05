43a847d902256016a6d40e15e9066b6ea38f2d9a
eve-git <54647458+eve-git@users.noreply.github.com>
2023-12-05 14:35:43 -0800

    18201 Fix error after pressing Incorporate now (#749)
    * fixed the bug
    
    * update version
    
    * update according to review

--- src/App.vue
+++ src/App.vue
@@ -275,8 +275,7 @@ export default class App extends Mixins(
 
     // if there is stored legal type for an IA then incorporate/register it now
     const legaltype = sessionStorage.getItem('LEGAL_TYPE')
-
-    if (legaltype) {
+    if (legaltype && this.isAuthenticated) {
       try {
         if (this.isNewBusiness) {
           await this.incorporateNow(legaltype as CorpTypeCd)
