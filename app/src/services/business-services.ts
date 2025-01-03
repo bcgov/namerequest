b394b94f5ff1655cebf2e918a273f6f948bad98c
meawong <megan.a.wong@gov.bc.ca>
2025-01-03 13:06:27 -0800

    25066 - Fix BEN Conversion for Unauthorized Users (#791)
    * 25066-Update-allowable-BEN-conversions
    
    * 25066-Lint fixes
    
    * 25066-Use-namexservice-to-fetch-business
    
    * Add logic for displaying name change button
    
    * update patch version
    
    * Add feature flag check
    
    * lint fix
    
    * 24205 - updates for name change and alteration (#784)
    
    * 24205 - updates for name change and alteration ff
    
    * 24205 - remove feature flags
    
    * 24205 - remove feature flags
    
    * 24205 - update version
    
    * 24205 - remove unsused import
    
    * 24205 - remove consitional chaining
    
    * 24205 - update funstions
    
    * 24205
    
    * 24205
    
    * 24205- update mounted() with async
    
    * 24605 additional address field error when editing nr (#785)
    
    * 23332 - Enable request restoration NR for a limited restored company (#786)
    
    * Added check for limited restoration
    
    * Added condition to use exact look up (BusinessFetch) component for restoration NRs
    
    * 23332 - Fix request restoration nr for limited restored company (#787)
    
    * fix: allow historical businesses to request restoration NR
    
    * 24841 display error when create payment fails (#788)
    
    * 25066-Update-allowable-BEN-conversions
    
    * 25066-Lint fixes
    
    * 25066-Use-namexservice-to-fetch-business
    
    * 25066-Remove-unecessary-import
    
    * 25066-Remove fetch business service that is unused
    
    * 25066-Update patch version
    
    ---------
    
    Co-authored-by: ketaki-deodhar <116035339+ketaki-deodhar@users.noreply.github.com>
    Co-authored-by: EPortman <169073732+EPortman@users.noreply.github.com>
    Co-authored-by: Aimee <Aimee.Gao@gov.bc.ca>

--- app/src/services/business-services.ts
+++ app/src/services/business-services.ts
@@ -1,7 +1,6 @@
 import Axios from 'axios'
 import { AddAxiosInterceptors } from '@/plugins'
 import { BusinessRequest } from '@/interfaces'
-import { NOT_FOUND } from 'http-status-codes'
 
 const axios = AddAxiosInterceptors(Axios.create())
 
@@ -17,27 +16,5 @@ export default class BusinessServices {
   static async createBusiness (businessRequest: BusinessRequest): Promise<any> {
     const url = `${BusinessServices.legalApiUrl}/businesses?draft=true`
     return axios.post(url, businessRequest)
-  }
-
-  /**
-   * Fetches a business record.
-   * Must be logged in to use this.
-   * Throws an exception on error.
-   */
-  static async fetchBusiness (identifier: string): Promise<any> {
-    const url = `${BusinessServices.legalApiUrl}/businesses/${identifier}`
-    return axios.get(url)
-      .then(response => {
-        if (response.data?.business) {
-          return response.data?.business
-        }
-        console.log('fetchBusiness() error - invalid response =', response)
-        throw new Error('Invalid API response')
-      }).catch(error => {
-        if (error?.response?.status === NOT_FOUND) {
-          return null // Business not found (not an error)
-        }
-        throw error
-      })
   }
 }
