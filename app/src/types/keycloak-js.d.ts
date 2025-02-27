a0b164b1ad9facb65d9857582541140e72a116a0
Travis Semple <travis8814@gmail.com>
2025-02-26 18:36:11 -0800

    linting / exclusion

--- app/src/types/keycloak-js.d.ts
+++ app/src/types/keycloak-js.d.ts
@@ -26,7 +26,7 @@ declare module 'keycloak-js' {
   export type KeycloakResponseType = 'code'|'id_token token'|'code id_token token';
   export type KeycloakFlow = 'standard'|'implicit'|'hybrid';
   export type KeycloakPkceMethod = 'S256' | false;
-  
+
   export interface KeycloakConfig {
     /**
      * URL to the Keycloak server, for example: http://keycloak-server/auth
@@ -41,7 +41,7 @@ declare module 'keycloak-js' {
      */
     clientId: string;
   }
-  
+
   export interface Acr {
     /**
      * Array of values, which will be used inside ID Token `acr` claim sent inside the `claims` parameter to Keycloak server during login.
@@ -54,7 +54,7 @@ declare module 'keycloak-js' {
      */
     essential: boolean;
   }
-  
+
   export interface KeycloakInitOptions {
     /**
      * Adds a [cryptographic nonce](https://en.wikipedia.org/wiki/Cryptographic_nonce)
@@ -62,9 +62,9 @@ declare module 'keycloak-js' {
      * @default true
      */
     useNonce?: boolean;
-  
+
     /**
-     * 
+     *
      * Allow usage of different types of adapters or a custom adapter to make Keycloak work in different environments.
      *
      * The following options are supported:
@@ -92,46 +92,46 @@ declare module 'keycloak-js' {
      * ```
      */
     adapter?: 'default' | 'cordova' | 'cordova-native' | KeycloakAdapter;
-    
+
     /**
      * Specifies an action to do on load.
      */
     onLoad?: KeycloakOnLoad;
-  
+
     /**
      * Set an initial value for the token.
      */
     token?: string;
-  
+
     /**
      * Set an initial value for the refresh token.
      */
     refreshToken?: string;
-  
+
     /**
      * Set an initial value for the id token (only together with `token` or
      * `refreshToken`).
      */
     idToken?: string;
-  
+
     /**
      * Set an initial value for skew between local time and Keycloak server in
      * seconds (only together with `token` or `refreshToken`).
      */
     timeSkew?: number;
-  
+
     /**
      * Set to enable/disable monitoring login state.
      * @default true
      */
     checkLoginIframe?: boolean;
-  
+
     /**
      * Set the interval to check login state (in seconds).
      * @default 5
      */
     checkLoginIframeInterval?: number;
-  
+
     /**
      * Set the OpenID Connect response mode to send to Keycloak upon login.
      * @default fragment After successful authentication Keycloak will redirect
@@ -140,59 +140,59 @@ declare module 'keycloak-js' {
      *                   recommended over query.
      */
     responseMode?: KeycloakResponseMode;
-  
+
     /**
      * Specifies a default uri to redirect to after login or logout.
      * This is currently supported for adapter 'cordova-native' and 'default'
      */
     redirectUri?: string;
-  
+
     /**
      * Specifies an uri to redirect to after silent check-sso.
      * Silent check-sso will only happen, when this redirect uri is given and
      * the specified uri is available within the application.
      */
     silentCheckSsoRedirectUri?: string;
-  
+
     /**
      * Specifies whether the silent check-sso should fallback to "non-silent"
      * check-sso when 3rd party cookies are blocked by the browser. Defaults
      * to true.
      */
     silentCheckSsoFallback?: boolean;
-  
+
     /**
      * Set the OpenID Connect flow.
      * @default standard
      */
     flow?: KeycloakFlow;
-  
+
     /**
      * Configures the Proof Key for Code Exchange (PKCE) method to use. This will default to 'S256'.
      * Can be disabled by passing `false`.
      */
     pkceMethod?: KeycloakPkceMethod;
-  
+
     /**
      * Configures the 'acr_values' query param in compliance with section 3.1.2.1
      * of the OIDC 1.0 specification.
      * Used to tell Keycloak what level of authentication the user needs.
      */
     acrValues?: string;
-  
+
     /**
      * Enables logging messages from Keycloak to the console.
      * @default false
      */
     enableLogging?: boolean
-  
+
     /**
-     * Set the default scope parameter to the login endpoint. Use a space-delimited list of scopes. 
+     * Set the default scope parameter to the login endpoint. Use a space-delimited list of scopes.
      * Note that the scope 'openid' will be always be added to the list of scopes by the adapter.
      * Note that the default scope specified here is overwritten if the `login()` options specify scope explicitly.
      */
     scope?: string
-    
+
     /**
      * Configures how long will Keycloak adapter wait for receiving messages from server in ms. This is used,
      * for example, when waiting for response of 3rd party cookies check.
@@ -200,31 +200,31 @@ declare module 'keycloak-js' {
      * @default 10000
      */
     messageReceiveTimeout?: number
-  
+
     /**
      * When onLoad is 'login-required', sets the 'ui_locales' query param in compliance with section 3.1.2.1
      * of the OIDC 1.0 specification.
      */
     locale?: string;
-  
+
     /**
      * HTTP method for calling the end_session endpoint. Defaults to 'GET'.
      */
     logoutMethod?: 'GET' | 'POST';
   }
-  
+
   export interface KeycloakLoginOptions {
     /**
      * Specifies the scope parameter for the login url
      * The scope 'openid' will be added to the scope if it is missing or undefined.
      */
     scope?: string;
-  
+
     /**
      * Specifies the uri to redirect to after login.
      */
     redirectUri?: string;
-  
+
     /**
      * By default the login screen is displayed if the user is not logged into
      * Keycloak. To only authenticate to the application if the user is already
@@ -235,13 +235,13 @@ declare module 'keycloak-js' {
      * even if it has been given previously.
      */
     prompt?: 'none' | 'login' | 'consent';
-  
+
     /**
      * If value is `'register'` then user is redirected to registration page,
      * otherwise to login page.
      */
     action?: string;
-  
+
     /**
      * Used just if user is already authenticated. Specifies maximum time since
      * the authentication of user happened. If user is already authenticated for
@@ -249,35 +249,35 @@ declare module 'keycloak-js' {
      * authenticate again.
      */
     maxAge?: number;
-  
+
     /**
      * Used to pre-fill the username/email field on the login form.
      */
     loginHint?: string;
-  
+
     /**
      * Sets the `acr` claim of the ID token sent inside the `claims` parameter. See section 5.5.1 of the OIDC 1.0 specification.
      */
     acr?: Acr;
-  
+
     /**
      * Configures the 'acr_values' query param in compliance with section 3.1.2.1
      * of the OIDC 1.0 specification.
      * Used to tell Keycloak what level of authentication the user needs.
      */
     acrValues?: string;
-  
+
     /**
      * Used to tell Keycloak which IDP the user wants to authenticate with.
      */
     idpHint?: string;
-  
+
           /**
      * Sets the 'ui_locales' query param in compliance with section 3.1.2.1
                  * of the OIDC 1.0 specification.
      */
     locale?: string;
-  
+
     /**
      * Specifies arguments that are passed to the Cordova in-app-browser (if applicable).
      * Options 'hidden' and 'location' are not affected by these arguments.
@@ -286,32 +286,32 @@ declare module 'keycloak-js' {
      */
     cordovaOptions?: { [optionName: string]: string };
   }
-  
+
   export interface KeycloakLogoutOptions {
     /**
      * Specifies the uri to redirect to after logout.
      */
     redirectUri?: string;
-  
+
     /**
      * HTTP method for calling the end_session endpoint. Defaults to 'GET'.
      */
     logoutMethod?: 'GET' | 'POST';
   }
-  
+
   export interface KeycloakRegisterOptions extends Omit<KeycloakLoginOptions, 'action'> { }
-  
+
   export interface KeycloakAccountOptions {
     /**
      * Specifies the uri to redirect to when redirecting back to the application.
      */
-    redirectUri?: string;	
+    redirectUri?: string;
   }
   export interface KeycloakError {
     error: string;
     error_description: string;
   }
-  
+
   export interface KeycloakAdapter {
     login(options?: KeycloakLoginOptions): Promise<void>;
     logout(options?: KeycloakLogoutOptions): Promise<void>;
@@ -319,7 +319,7 @@ declare module 'keycloak-js' {
     accountManagement(): Promise<void>;
     redirectUri(options: { redirectUri: string; }, encodeHash: boolean): string;
   }
-  
+
   export interface KeycloakProfile {
     id?: string;
     username?: string;
@@ -332,7 +332,7 @@ declare module 'keycloak-js' {
     createdTimestamp?: number;
     attributes?: Record<string, unknown>;
   }
-  
+
   export interface KeycloakTokenParsed {
     iss?: string;
     sub?: string;
@@ -349,20 +349,20 @@ declare module 'keycloak-js' {
     resource_access?: KeycloakResourceAccess;
     [key: string]: any; // Add other attributes here.
   }
-  
+
   export interface KeycloakResourceAccess {
     [key: string]: KeycloakRoles
   }
-  
+
   export interface KeycloakRoles {
     roles: string[];
   }
-  
+
   /**
    * @deprecated Instead of importing 'KeycloakInstance' you can import 'Keycloak' directly as a type.
    */
   export type KeycloakInstance = Keycloak;
-  
+
   /**
    * A client for the Keycloak authentication server.
    * @see {@link https://keycloak.gitbooks.io/securing-client-applications-guide/content/topics/oidc/javascript-adapter.html|Keycloak JS adapter documentation}
@@ -373,158 +373,158 @@ declare module 'keycloak-js' {
      * @param config A configuration object or path to a JSON config file.
      */
     constructor(config: KeycloakConfig | string)
-  
+
     /**
      * Is true if the user is authenticated, false otherwise.
      */
     authenticated?: boolean;
-  
+
     /**
     * The user id.
     */
     subject?: string;
-  
+
     /**
     * Response mode passed in init (default value is `'fragment'`).
     */
     responseMode?: KeycloakResponseMode;
-  
+
     /**
     * Response type sent to Keycloak with login requests. This is determined
     * based on the flow value used during initialization, but can be overridden
     * by setting this value.
     */
     responseType?: KeycloakResponseType;
-  
+
     /**
     * Flow passed in init.
     */
     flow?: KeycloakFlow;
-  
+
     /**
     * The realm roles associated with the token.
     */
     realmAccess?: KeycloakRoles;
-  
+
     /**
     * The resource roles associated with the token.
     */
     resourceAccess?: KeycloakResourceAccess;
-  
+
     /**
     * The base64 encoded token that can be sent in the Authorization header in
     * requests to services.
     */
     token?: string;
-  
+
     /**
     * The parsed token as a JavaScript object.
     */
     tokenParsed?: KeycloakTokenParsed;
-  
+
     /**
     * The base64 encoded refresh token that can be used to retrieve a new token.
     */
     refreshToken?: string;
-  
+
     /**
     * The parsed refresh token as a JavaScript object.
     */
     refreshTokenParsed?: KeycloakTokenParsed;
-  
+
     /**
     * The base64 encoded ID token.
     */
     idToken?: string;
-  
+
     /**
     * The parsed id token as a JavaScript object.
     */
     idTokenParsed?: KeycloakTokenParsed;
-  
+
     /**
     * The estimated time difference between the browser time and the Keycloak
     * server in seconds. This value is just an estimation, but is accurate
     * enough when determining if a token is expired or not.
     */
     timeSkew?: number;
-  
+
     /**
     * Whether the instance has been initialized by calling `.init()`.
     */
     didInitialize: boolean;
-    
+
     /**
     * @private Undocumented.
     */
     loginRequired?: boolean;
-  
+
     /**
     * @private Undocumented.
     */
     authServerUrl?: string;
-  
+
     /**
     * @private Undocumented.
     */
     realm?: string;
-  
+
     /**
     * @private Undocumented.
     */
     clientId?: string;
-  
+
     /**
     * @private Undocumented.
     */
     redirectUri?: string;
-  
+
     /**
     * @private Undocumented.
     */
     sessionId?: string;
-  
+
     /**
     * @private Undocumented.
     */
     profile?: KeycloakProfile;
-  
+
     /**
     * @private Undocumented.
     */
     userInfo?: {}; // KeycloakUserInfo;
-  
+
     /**
     * Called when the adapter is initialized.
     */
     onReady?(authenticated?: boolean): void;
-  
+
     /**
     * Called when a user is successfully authenticated.
     */
     onAuthSuccess?(): void;
-  
+
     /**
     * Called if there was an error during authentication.
     */
     onAuthError?(errorData: KeycloakError): void;
-  
+
     /**
     * Called when the token is refreshed.
     */
     onAuthRefreshSuccess?(): void;
-  
+
     /**
     * Called if there was an error while trying to refresh the token.
     */
     onAuthRefreshError?(): void;
-  
+
     /**
     * Called if the user is logged out (will only be called if the session
     * status iframe is enabled, or in Cordova mode).
     */
     onAuthLogout?(): void;
-  
+
     /**
     * Called when the access token is expired. If a refresh token is available
     * the token can be refreshed with Keycloak#updateToken, or in cases where
@@ -532,75 +532,75 @@ declare module 'keycloak-js' {
     * obtain a new access token.
     */
     onTokenExpired?(): void;
-  
+
     /**
     * Called when a AIA has been requested by the application.
     * @param status the outcome of the required action
     * @param action the alias name of the required action, e.g. UPDATE_PASSWORD, CONFIGURE_TOTP etc.
     */
     onActionUpdate?(status: 'success'|'cancelled'|'error', action?: string): void;
-  
+
     /**
     * Called to initialize the adapter.
     * @param initOptions Initialization options.
     * @returns A promise to set functions to be invoked on success or error.
     */
     init(initOptions?: KeycloakInitOptions): Promise<boolean>;
-  
+
     /**
     * Redirects to login form.
     * @param options Login options.
     */
     login(options?: KeycloakLoginOptions): Promise<void>;
-  
+
     /**
     * Redirects to logout.
     * @param options Logout options.
     */
     logout(options?: KeycloakLogoutOptions): Promise<void>;
-  
+
     /**
     * Redirects to registration form.
     * @param options The options used for the registration.
     */
     register(options?: KeycloakRegisterOptions): Promise<void>;
-  
+
     /**
     * Redirects to the Account Management Console.
     */
     accountManagement(): Promise<void>;
-  
+
     /**
     * Returns the URL to login form.
     * @param options Supports same options as Keycloak#login.
     */
     createLoginUrl(options?: KeycloakLoginOptions): Promise<string>;
-  
+
     /**
     * Returns the URL to logout the user.
     * @param options Logout options.
     */
     createLogoutUrl(options?: KeycloakLogoutOptions): string;
-  
+
     /**
     * Returns the URL to registration page.
     * @param options The options used for creating the registration URL.
     */
     createRegisterUrl(options?: KeycloakRegisterOptions): Promise<string>;
-  
+
     /**
     * Returns the URL to the Account Management Console.
     * @param options The options used for creating the account URL.
     */
     createAccountUrl(options?: KeycloakAccountOptions): string;
-  
+
     /**
     * Returns true if the token has less than `minValidity` seconds left before
     * it expires.
     * @param minValidity If not specified, `0` is used.
     */
     isTokenExpired(minValidity?: number): boolean;
-  
+
     /**
     * If the token expires within `minValidity` seconds, the token is refreshed.
     * If the session status iframe is enabled, the session status is also
@@ -621,7 +621,7 @@ declare module 'keycloak-js' {
     * });
     */
     updateToken(minValidity?: number): Promise<boolean>;
-  
+
     /**
     * Clears authentication state, including tokens. This can be useful if
     * the application has detected the session was expired, for example if
@@ -629,32 +629,31 @@ declare module 'keycloak-js' {
     * callback listener being invoked.
     */
     clearToken(): void;
-  
+
     /**
     * Returns true if the token has the given realm role.
     * @param role A realm role name.
     */
     hasRealmRole(role: string): boolean;
-  
+
     /**
     * Returns true if the token has the given role for the resource.
     * @param role A role name.
     * @param resource If not specified, `clientId` is used.
     */
     hasResourceRole(role: string, resource?: string): boolean;
-  
+
     /**
     * Loads the user's profile.
     * @returns A promise to set functions to be invoked on success or error.
     */
     loadUserProfile(): Promise<KeycloakProfile>;
-  
+
     /**
     * @private Undocumented.
     */
     loadUserInfo(): Promise<{}>;
   }
-  
-  export default Keycloak;
+
+  export default Keycloak
   }
-  
