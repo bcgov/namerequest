2411bf19249365c58968eb2edd7e83bb12939ebe
bobby <bobbyjin935@gmail.com>
2025-07-18 08:46:19 -0700

    fix lint warning

--- app/tests/unit/applicant-info-2.spec.ts
+++ app/tests/unit/applicant-info-2.spec.ts
@@ -1,4 +1,4 @@
-import ApplicantInfo2 from '@/components/common/applicant-info-2.vue'
+// import ApplicantInfo2 from '@/components/common/applicant-info-2.vue'
 import { createLocalVue, shallowMount, mount } from '@vue/test-utils'
 // import module from '@/store/new-request-module'
 import Vuetify from 'vuetify'
