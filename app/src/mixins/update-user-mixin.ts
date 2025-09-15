d398bab79dde10b4a63c345f58d973680b5d555e
Severin Beauvais <severin.beauvais@gov.bc.ca>
2025-09-15 16:09:12 -0700

    - app version = 5.6.0 - imported latest LD JS client SDK v3.9.0 - updated UpdateUserMixin to create contexts - updated featureFlags module to use contexts

--- app/src/mixins/update-user-mixin.ts
+++ app/src/mixins/update-user-mixin.ts
@@ -1,35 +1,53 @@
 import { Component, Vue } from 'vue-property-decorator'
-import { UpdateLdUser } from '@/plugins'
+import { getKeycloakRoles, UpdateLdUser } from '@/plugins'
 import AuthServices from '@/services/auth-services'
+import { SessionStorageKeys } from 'sbc-common-components/src/util/constants'
 
 @Component({})
 export class UpdateUserMixin extends Vue {
-  /** Fetches the user's info and updates LaunchDarkly. */
+  /** Fetches the user and org info and updates LaunchDarkly. */
   async updateUser (): Promise<any> {
     // don't run in Jest tests
     if (process.env.JEST_WORKER_ID) return
 
     try {
-      const userInfo = await AuthServices.fetchUserInfo()
-      if (userInfo) {
-        await this.updateLaunchDarkly(userInfo)
-      }
+      // get user info
+      const userInfo = await AuthServices.fetchUserInfo().catch(() => null)
+
+      // get user roles
+      const userRoles = getKeycloakRoles()
+
+      // get organization info
+      const currentAccount = sessionStorage.getItem(SessionStorageKeys.CurrentAccount)
+      const accountId = currentAccount && JSON.parse(currentAccount)?.id
+      const orgInfo = accountId && await AuthServices.fetchOrgInfo(accountId).catch(() => null)
+
+      await this.updateLaunchDarkly(userInfo, userRoles, orgInfo)
     } catch (err) {
       // just log the error -- no need to halt app
       console.log('Error updating user =', err) // eslint-disable-line no-console
     }
   }
 
-  /** Updates Launch Darkly with current user info. */
-  private async updateLaunchDarkly (userInfo: any): Promise<void> {
-    // since username is unique, use it as the user key
-    const key = userInfo.username as string
-    const email = userInfo.contacts[0]?.email || userInfo.email as string
-    const firstName = userInfo?.firstname as string
-    const lastName = userInfo?.lastname as string
-    // remove leading { and trailing } and tokenize string
-    const custom: any = { roles: userInfo.roles?.slice(1, -1).split(',') }
+  /** Updates Launch Darkly with current user and org contexts. */
+  private async updateLaunchDarkly (userInfo = null, userRoles = null, orgInfo = null): Promise<void> {
+    const userContext = userInfo && {
+      kind: 'user',
+      // since username is unique, use it as the user key
+      key: userInfo.username,
+      // if we can't get contact email then use user email
+      email: userInfo.contacts[0]?.email || userInfo.email,
+      firstName: userInfo?.firstname,
+      lastName: userInfo?.lastname,
+      roles: userRoles || []
+    }
 
-    return UpdateLdUser(key, email, firstName, lastName, custom)
+    const orgContext = orgInfo && {
+      kind: 'organization',
+      key: orgInfo.id.toString(),
+      name: orgInfo.name
+    }
+
+    return UpdateLdUser(userContext, orgContext)
   }
 }
