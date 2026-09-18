6af5106b353c19528549433a2d8a6573d812c8e2
Kial Jinnah <kialj876@gmail.com>
2026-09-18 13:07:13 -0400

    34969 - help me choose modal tweak
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- app/src/components/dialogs/numbered-company-help.vue
+++ app/src/components/dialogs/numbered-company-help.vue
@@ -48,7 +48,6 @@
                 <ul>
                   <li>combine or merge your business with another business (Amalgamation)</li>
                   <li>reactivate a business (Restoration)</li>
-                  <li>move your business out of B.C. (Continuation-out)</li>
                 </ul>
                 <p class="font-weight-bold">
                   Note: all businesses in Corporate Online will be moved to the new BC Business Registry soon
@@ -108,6 +107,7 @@
                   <li>change your business address when you move</li>
                   <li>change directors names or addresses</li>
                   <li>change basic business information (Alteration)</li>
+                  <li>move your business out of B.C. (Continuation-out)</li>
                 </ul>
                 <v-btn
                   id="help-business-registry-btn"
